5325dcaeeb10c166fcc154dd9a940a6a7cd43e14
AntixxTechHub <info@antixxtechhub.com>
2023-08-25 16:26:45 +0530

    new code

---
+++ pages/blog-details/_slug/index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <NavbarStyleTwo />
+        <PageTitle v-if="details !== null" :pageTitle="details[0].attributes.title" pageDesc="News and Insights" />
+        <div v-if="details !== null">
+            <BlogDetails v-bind:detailsContent="details" />
+        </div>
+        <DealDoxFooter />
+    </div>
+</template>
+
+<script>
+import Navbar from '../../../layouts/Navbar';
+import PageTitle from '../../../components/Common/PageTitle';
+import BlogDetails from '../../../components/BlogTwo/BlogDetails'
+import DealDoxFooter from '../../../layouts/Footer'
+import axios from 'axios';
+
+export default {
+    components: {
+        Navbar,
+        PageTitle,
+        BlogDetails,
+        DealDoxFooter,
+    },
+
+    data() {
+        return {
+            details: null,
+            seoData: null,
+        }
+    },
+
+    created: async function () {
+        const { slug } = this.$route.params
+        const reaponse = await axios.get(`https://evolvestrapi.pbwebvision.in/api/blogs?filters[slug][$eq]=${slug}&populate=*`, { params: { slug } })
+        this.details = reaponse.data.data;
+        const pageData = this.details.length > 0 ? this.details[0] : {};
+        if (pageData?.attributes?.seo) {
+            this.seoData = pageData.attributes.seo;
+        }
+    },
+    head({ $seo }) {
+
+        return $seo({
+            title: this.seoData?.metaTitle,
+            description: this.seoData?.metaDescription,
+            keywords: this.seoData?.keywords,
+        });
+    },
+};
+</script>
