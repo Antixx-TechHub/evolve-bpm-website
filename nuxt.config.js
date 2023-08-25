342fa46c1721506bf04aebe313a4f42aeff1674d
AntixxTechHub <info@antixxtechhub.com>
2023-08-25 14:25:21 +0530

    Final changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'DealDox',
+    title: 'Evolve BPM',
     htmlAttrs: {
       lang: 'en-us'
     },
