20d9ef031d010ee5ee36c8c36ab1ff0cbf20a259
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-08-26 17:26:22 +0530

    team API is working fine

--- components/HomeEight/OurTeam.vue
+++ components/HomeEight/OurTeam.vue
@@ -6,27 +6,28 @@
                 <h2>We Help to Acheive Your Business Goal</h2>
             </div>
 
-            <div class="row justify-content-center">
-                <div class="col-lg-3 col-sm-6">
+            <div class="row justify-content-center" v-if="teams !== null">
+                <div class="col-lg-3 col-sm-6" v-for="team in teams.singleTeam" :key="team.id">
                     <div class="rivate-team-card">
                         <div class="team-image">
-                            <img src="~/assets/images/more-home/team/team-1.png" alt="image">
+                            <img :src="team.image.data.attributes.url" alt="teams">
 
                             <ul class="social-link">
-                                <li><a href="#" class="d-block" target="_blank"><i class='bx bxl-facebook'></i></a></li>
+                                <!-- <li><a href="#" class="d-block" target="_blank"><i class='bx bxl-facebook'></i></a></li>
                                 <li><a href="#" class="d-block" target="_blank"><i class='bx bxl-twitter'></i></a></li>
-                                <li><a href="#" class="d-block" target="_blank"><i class='bx bxl-instagram'></i></a></li>
-                                <li><a href="#" class="d-block" target="_blank"><i class='bx bxl-linkedin'></i></a></li>
+                                <li><a href="#" class="d-block" target="_blank"><i class='bx bxl-instagram'></i></a></li> -->
+                                <li><a :href="team.linkedin" class="d-block" target="_blank"><i class='bx bxl-linkedin'></i></a></li>
                             </ul>
                         </div>
                         <div class="team-content">
-                            <h3>James Anderson</h3>
-                            <span>CEO</span>
+                            <h3>{{ team.title }}</h3>
+                            <span>{{ team.designation }}</span>
+                            <!-- <p>{{ team.shortDesc }}</p> -->
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-sm-6">
+                <!-- <div class="col-lg-3 col-sm-6">
                     <div class="rivate-team-card">
                         <div class="team-image">
                             <img src="~/assets/images/more-home/team/team-2.png" alt="image">
@@ -97,7 +98,7 @@
                             <span>Support</span>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
         <div class="rivate-team-bottom-text">
@@ -107,7 +108,20 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: 'OurTeam'
+    name: 'OurTeam',
+    data() {
+        return {
+            teams: null,
+        }
+    },
+    created: async function () {
+        const response = await axios.get('https://evolvestrapi.pbwebvision.in/api/team?populate=deep,5')
+        const { data: { attributes } } = response.data
+        this.teams = attributes
+    },
+
 }
 </script>
