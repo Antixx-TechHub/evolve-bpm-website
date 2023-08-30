88d66d9d9d562eb56e23f7cac8c2a7aa6ed30b4e
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-08-30 18:01:36 +0530

    api added seo

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -20,6 +20,8 @@
     import OurClientsAreSaying from '../components/HomeEight/OurClientsAreSaying'
     import Partner from '../components/Common/Partner'
     import Footer from '../layouts/Footer'
+    import axios from 'axios';
+
 
     export default {
         components: {
@@ -31,6 +33,25 @@
             OurClientsAreSaying,
             Partner,
             Footer,
+        },
+        data() {
+        return {
+            seoData: [],
         }
-    }
+    },
+    created: async function () {
+        const response = await axios.get('http://localhost:1337/api/pages?filters[slug][$eq]=about-us&populate=deep,5')
+        const pageData = response.data.data?.length > 0 ? response.data.data[0] : {};
+        if (pageData?.attributes?.seo?.length > 0) {
+            this.seoData = pageData.attributes.seo[0];
+        }
+    },
+    head({ $seo }) {
+        return $seo({
+            title: this.seoData.metaTitle,
+            description: this.seoData.metaDescription,
+            keywords: this.seoData.keywords,
+        });
+    },
+}
 </script>
--- pages/services.vue
+++ pages/services.vue
@@ -12,6 +12,8 @@
     import PageTitle from '../components/Common/PageTitle'
     import ServicesContent from '../components/ServicesOne/ServicesContent'
     import FooterTwo from '../layouts/Footer'
+    import axios from 'axios';
+
 
     export default {
         components: {
@@ -19,6 +21,26 @@
             PageTitle,
             ServicesContent,
             FooterTwo,
+        },
+        data() {
+        return {
+            seoData: [],
         }
-    }
+    },
+    created: async function () {
+        const response = await axios.get('http://localhost:1337/api/pages?filters[slug][$eq]=services-1&populate=deep,5')
+        const pageData = response.data.data?.length > 0 ? response.data.data[0] : {};
+        if (pageData?.attributes?.seo?.length > 0) {
+            this.seoData = pageData.attributes.seo[0];
+        }
+    },
+    head({ $seo }) {
+        return $seo({
+            title: this.seoData.metaTitle,
+            description: this.seoData.metaDescription,
+            keywords: this.seoData.keywords,
+            // image: this.post.image || '',
+        });
+    },
+}
 </script>
