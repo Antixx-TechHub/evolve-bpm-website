293b6feb4ab7b9b09a3ce4c0f64740b77de9e9f1
Srohanrs <prashant.bhosale@antixxtechhub.com>
2023-08-30 14:34:40 +0530

    seo code added

--- pages/portpolio-details/_slug/index.vue
+++ pages/portpolio-details/_slug/index.vue
@@ -26,12 +26,26 @@ export default {
     data() {
         return {
             details: null,
+            seoData: null,
+
         }
     },
     created: async function () {
         const { slug } = this.$route.params
         const reaponse = await axios.get(`https://evolvestrapi.pbwebvision.in/api/portfolios?filters[slug][$eq]=${slug}&populate=*`, { params: { slug } })
         this.details = reaponse.data.data.sort((b, a) => a.id - b.id);
+        const pageData = this.details.length > 0 ? this.details[0] : {};
+        if (pageData?.attributes?.seo) {
+            this.seoData = pageData.attributes.seo;
+        }
+    },
+    head({ $seo }) {
+
+        return $seo({
+            title: this.seoData?.metaTitle,
+            description: this.seoData?.metaDescription,
+            keywords: this.seoData?.keywords,
+        });
     },
 };
 </script>
